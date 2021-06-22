52fb3236ebe0b72a0443642b77ffe1fa53ea8ce1
Andrey Chernyshev <andrey.chernyshev@simbirsoft.com>
2021-06-22 20:25:27 +0300

    build 1

---
+++ HESI/Classes/Misc/Appearance.swift
@@ -0,0 +1,21 @@
+//
+//  Appearance.swift
+//  Ged
+//
+//  Created by Andrey Chernyshev on 23.04.2021.
+//
+
+import UIKit
+
+struct Appearance {
+    static let backgroundColor = UIColor(integralRed: 255, green: 254, blue: 247)
+    
+    static let mainColor = UIColor(integralRed: 247, green: 203, blue: 73)
+    static let mainColorAlpha = UIColor(integralRed: 247, green: 203, blue: 73, alpha: 0.3)
+    
+    static let takeFreeTestColor = UIColor(integralRed: 233, green: 69, blue: 98)
+    
+    static let progress1Color = UIColor(integralRed: 247, green: 203, blue: 73)
+    static let progress2Color = UIColor(integralRed: 233, green: 69, blue: 98)
+    static let progress3Color = UIColor(integralRed: 148, green: 52, blue: 237)
+}
