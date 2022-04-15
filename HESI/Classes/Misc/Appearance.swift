e31a683c7146a65730b7b93417760725d85a9f19
akonst17 <akonst17@gmail.com>
2022-04-15 11:14:59 +0300

    upgrade; build 8

--- HESI/Classes/Misc/Appearance.swift
+++ HESI/Classes/Misc/Appearance.swift
@@ -18,4 +18,8 @@ struct Appearance {
     static let progress1Color = UIColor(integralRed: 247, green: 203, blue: 71)
     static let progress2Color = UIColor(integralRed: 233, green: 69, blue: 98)
     static let progress3Color = UIColor(integralRed: 148, green: 52, blue: 237)
+    
+    static let successColor = UIColor(integralRed: 46, green: 190, blue: 161)
+    static let errorColor = UIColor(integralRed: 255, green: 115, blue: 99)
+    static let warningColor = UIColor(integralRed: 255, green: 155, blue: 38)
 }
