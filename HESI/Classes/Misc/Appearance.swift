938b2f2ed7e2293466d911de83e54a03c6474005
akonst17 <akonst17@gmail.com>
2022-08-02 16:50:09 +0300

    обновления Nursing; build 9

--- HESI/Classes/Misc/Appearance.swift
+++ HESI/Classes/Misc/Appearance.swift
@@ -8,18 +8,14 @@
 import UIKit
 
 struct Appearance {
-    static let backgroundColor = UIColor(integralRed: 255, green: 254, blue: 247)
-    
-    static let mainColor = UIColor(integralRed: 247, green: 203, blue: 71)
-    static let mainColorAlpha = UIColor(integralRed: 247, green: 203, blue: 71, alpha: 0.3)
-    
-    static let takeFreeTestColor = UIColor(integralRed: 233, green: 69, blue: 98)
-    
-    static let progress1Color = UIColor(integralRed: 247, green: 203, blue: 71)
-    static let progress2Color = UIColor(integralRed: 233, green: 69, blue: 98)
-    static let progress3Color = UIColor(integralRed: 148, green: 52, blue: 237)
-    
+    static let mainColor = UIColor(integralRed: 247, green: 203, blue: 73)
+    static let mainColorAlpha = UIColor(integralRed: 247, green: 203, blue: 73, alpha: 0.3)
+    static let secondColor = UIColor(integralRed: 148, green: 52, blue: 237)
+    static let thirdColor = UIColor(integralRed: 233, green: 69, blue: 98)
     static let successColor = UIColor(integralRed: 46, green: 190, blue: 161)
     static let errorColor = UIColor(integralRed: 255, green: 115, blue: 99)
     static let warningColor = UIColor(integralRed: 255, green: 155, blue: 38)
+    static let backgroundColor = UIColor(integralRed: 255, green: 254, blue: 247)
+    static let greyColor = UIColor(integralRed: 75, green: 81, blue: 102)
+    static let blackColor = UIColor(integralRed: 19, green: 24, blue: 42)
 }
